a09d1828cf1731402215e2dd8f4a24f07f46be7e
Sivan Mehta <Sivanmehta2@gmail.com>
2017-03-19 13:13:55 -0400

    scraped every meet that a person has swam at

--- scraping/pull-data.js
+++ scraping/pull-data.js
@@ -1,16 +1,30 @@
 const request = require('request')
-const htmlparser = require('htmlparser')
+const util = require('util')
 
-request.get({
-  url: 'https://www.collegeswimming.com/swimmer/233047/times/bymeet/',
-  headers: {
-    Referer: 'https://www.collegeswimming.com/swimmer/233047/',
-    Accept: '*/*',
-    Host: 'www.collegeswimming.com',
-    DNT: 1,
-    'X-Requested-With': 'XMLHttpRequest'
-  }
-}, (err, response, body) => {
-  // console.log(response.headers)
-  console.log(body)
-})
+const cheerio = require('cheerio')
+
+sivan = 233047
+brian = 210182
+
+function scrape(id) {
+  request.get({
+    url: 'https://www.collegeswimming.com/swimmer/' + id + '/times/bymeet/',
+    headers: {
+      Referer: 'https://www.collegeswimming.com/swimmer/' + id,
+      Accept: '*/*',
+      Host: 'www.collegeswimming.com',
+      DNT: 1,
+      'X-Requested-With': 'XMLHttpRequest'
+    }
+  }, (err, response, body) => {
+
+    var $ = cheerio.load(body, { ignoreWhitespace: true })
+    var meets = $("table.c-table-clean caption")
+
+    for(i = 0; i < meets.length; i ++) {
+      console.log(meets[i].children[0].data)
+    }
+  })
+}
+
+scrape(sivan)
