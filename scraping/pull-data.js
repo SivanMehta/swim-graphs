c36de4b81f435ea72b76796cecf8f2cdd70a4fb4
Sivan Mehta <Sivanmehta2@gmail.com>
2017-03-20 18:52:43 -0400

    scrape data and time for every swim

--- scraping/pull-data.js
+++ scraping/pull-data.js
@@ -13,16 +13,27 @@ function scrape(id) {
       Referer: 'https://www.collegeswimming.com/swimmer/' + id,
       Accept: '*/*',
       Host: 'www.collegeswimming.com',
-      DNT: 1,
       'X-Requested-With': 'XMLHttpRequest'
     }
   }, (err, response, body) => {
 
     var $ = cheerio.load(body, { ignoreWhitespace: true })
-    var meets = $("table.c-table-clean caption")
+    var meets = $("table.c-table-clean tbody tr")
 
-    for(i = 0; i < meets.length; i ++) {
-      console.log(meets[i].children[0].data)
+    for(var i = 0; i < meets.length; i++) {
+      const event = meets[i].children[1].children[0].data
+      const date = meets[i].children[3].children[0].data
+      try {
+        var time = meets[i].children[7].children[1].children[0].data
+      } catch (e) {
+        // user inputted time
+        time = "NA"
+      }
+      if(time == "(R)") { // formatting is different for relay lead offs
+        time = meets[i].children[7].children[3].children[0].data
+      }
+
+      console.log(event + "|" + date + "|" + time)
     }
   })
 }
