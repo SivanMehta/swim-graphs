d8ff389e9ff1a90a685733a2bfb7f0bb194749df
Sivan Mehta <Sivanmehta2@gmail.com>
2017-03-21 15:57:58 -0400

    export conversion utility

--- scraping/convert.js
+++ scraping/convert.js
@@ -1,12 +1,6 @@
-const women = require("./base-times-women.json")
 const moment = require('moment')
 
-function toSeconds(time) {
+exports.toSeconds(time) {
   const parsed = moment(time, ["ss.SS", "m:ss.SS", "mm:ss.SS"])
   return parsed.minutes() * 60 + parsed.seconds() + (parsed.milliseconds() / 1000)
 }
-Object.keys(women).map(key => {
-  women[key] = String(toSeconds(women[key]))
-})
-
-console.log(women)
