17e5eb83d2413828889d8fbd0cca91c033366d12
Sivan Mehta <Sivanmehta2@gmail.com>
2017-03-21 19:08:28 -0400

    Exclude seasons that have fewer than 50 swims
    These seasons are always filled with user-inputted
    times and almost never represent a swimmer's
    performance as a part of the actual team

--- processing/graphing.r
+++ processing/graphing.r
@@ -7,7 +7,13 @@ swims$date <- as.Date(swims$date)
 swims$date <- ymd(swims$date) + years(2017 - swims$season)
 swims$season <- as.factor(swims$season)
 
-ggplot(swims) + aes(x = date, y = points) +
-  geom_point(aes(group = season),
-                colour = "grey", size = .5) +
-  geom_smooth(se = FALSE)
+# exclude seasons with fewer than 50 swims (usually user-inputting times which aren't relevant)
+sufficient.seasons <- names(which(table(swims$season) > 50))
+sufficient.seasons.data <- swims[which(swims$season %in% sufficient.seasons),]
+
+ggplot(sufficient.seasons.data) + 
+  aes(x = date, y = points, colour = season) +
+  geom_point(colour = "grey", size = 1) +
+  geom_smooth(se = FALSE) +
+  scale_y_continuous(limits = c(300, 820))
+
