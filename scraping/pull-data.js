4c26a1cedba25543b68f3d7f49db0f59e233fa70
Sivan Mehta <Sivanmehta2@gmail.com>
2017-03-21 01:32:44 -0400

    added season to scraping

--- scraping/pull-data.js
+++ scraping/pull-data.js
@@ -1,7 +1,6 @@
 const request = require('request')
-const util = require('util')
-
 const cheerio = require('cheerio')
+const moment = require('moment')
 
 sivan = 233047
 brian = 210182
@@ -21,8 +20,15 @@ function scrape(id) {
     var meets = $("table.c-table-clean tbody tr")
 
     for(var i = 0; i < meets.length; i++) {
+      // what event is it? (we might not even use this, but is helpful)
       const event = meets[i].children[1].children[0].data
+
+      // process date into a season
       const date = meets[i].children[3].children[0].data
+      const month = moment(date, "MMM DD, YYYY").month()
+      const year = moment(date, "MMM DD, YYYY").year() + (month > 8 ? 1 : 0)
+
+      // read time
       try {
         var time = meets[i].children[7].children[1].children[0].data
       } catch (e) {
@@ -33,7 +39,7 @@ function scrape(id) {
         time = meets[i].children[7].children[3].children[0].data
       }
 
-      console.log(event + "|" + date + "|" + time)
+      console.log(event + "|" + date + "|" + time + "|" + (year - 1) + "-" + year)
     }
   })
 }
