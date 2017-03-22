1613b50406ef0e0a1cd2ce7df3d6f3b94bdba01c
Sivan Mehta <Sivanmehta2@gmail.com>
2017-03-21 20:14:15 -0400

    Add introduction to report
    - basic introduction to data
    - overall look at data
    - starting to pose some questions.

--- scraping/carnegie-mellon.js
+++ scraping/carnegie-mellon.js
@@ -12,5 +12,5 @@ const scrape = require('./pull-data')
 ids = [ 230365, 616062, 431239, 243692, 138305, 241049, 330478, 271394, 289068, 361980, 296605, 243606, 330393, 350978, 445698, 276955, 220606, 524473, 291138, 210182, 291810, 288434, 281950, 299734, 244205, 233047, 237152, 272478, 349336, 183988, 207663, 189790, 405637 ]
 scrapers = ids.map(id => done => scrape(id = id, done))
 
-console.log("date|time|season|points")
+console.log("event|date|time|season|points")
 async.waterfall(scrapers)
