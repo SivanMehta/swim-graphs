8f13ac9ebb13239d728076cf0374cc9df5ff41bd
Sivan Mehta <Sivanmehta2@gmail.com>
2017-03-21 21:43:31 -0400

    updated file names

--- processing/graphing.r
+++ processing/graphing.r
@@ -1,14 +1,15 @@
 library(ggplot2)
 library(lubridate)
 
-swims = read.csv("./data/cmu.csv", sep = "|", header=TRUE, row.names = NULL)
+swims = read.csv("./data/swims.csv", sep = "|", header=TRUE, row.names = NULL)
 
+# make each of the overlap by moving them to this year
 swims$date <- as.Date(swims$date)
 swims$date <- ymd(swims$date) + years(2017 - swims$season)
 swims$season <- as.factor(swims$season)
 
 # exclude seasons with fewer than 50 swims (usually user-inputting times which aren't relevant)
-sufficient.seasons <- names(which(table(swims$season) > 50))
+sufficient.seasons <- names(which(table(swims$season) > 100))
 sufficient.seasons.data <- swims[which(swims$season %in% sufficient.seasons),]
 
 ggplot(sufficient.seasons.data) + 
@@ -16,4 +17,3 @@ ggplot(sufficient.seasons.data) +
   geom_point(colour = "grey", size = 1) +
   geom_smooth(se = FALSE) +
   scale_y_continuous(limits = c(300, 820))
-
